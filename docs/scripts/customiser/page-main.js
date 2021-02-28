3dea189530ffd5a72a33fbbda17d478b334db926
Elttob <dan.bottlefact@gmail.com>
2021-02-28 14:02:07 +0000

    Load icondata and palettes json

--- docs/scripts/customiser/page-main.js
+++ docs/scripts/customiser/page-main.js
@@ -1,12 +1,21 @@
 import * as render from "./render.js"
 
+// Instead of using relative URLs, we're using absolute URLs for portability.
+const BASE_URL = "https://elttob.github.io/Vanilla-Dev/icons/"
+
+let icondata
+let palettes
+
 /**
  * Initialises the main page asynchronously.
  */
 export async function init() {
+	icondata = await fetch(BASE_URL + "icondata.json")
+	palettes = await fetch(BASE_URL + "palettes.json")
+
 	await render.init()
 
-	alert("Hello!")
+	alert("Loaded " + icondata.length + " icons and " + palettes.palettes.length + " palettes")
 
 	document.querySelector("#page-loading").className = "done"
 }
