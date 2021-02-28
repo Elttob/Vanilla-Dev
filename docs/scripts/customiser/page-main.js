ab04e41bba151a0890e2b1538f7c5c29bb9d873f
Elttob <dan.bottlefact@gmail.com>
2021-02-28 14:14:22 +0000

    Initial palette switching code

--- docs/scripts/customiser/page-main.js
+++ docs/scripts/customiser/page-main.js
@@ -6,16 +6,53 @@ const BASE_URL = "https://elttob.github.io/Vanilla-Dev/icons/"
 let icondata
 let palettes
 
+let activePalette
+
+/**
+ * Changes the currently active colour palette.
+ * @param {*} palette - The palette object to use.
+ */
+function setActivePalette(palette) {
+	document.documentElement.className = "theme-" + palette.page_colours
+	activePalette = palette
+
+	const paletteOptions = document.querySelectorAll("input[name=theme][type=radio]")
+
+	for(const paletteOption of paletteOptions) {
+		paletteOption.checked = paletteOption.value == palette.id
+	}
+}
+
+/**
+ * Initialises the palette options on the customiser page.
+ */
+function initPalettes() {
+	let userTheme = "light";
+	if(window.matchMedia != null && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		userTheme = "dark";
+	}
+
+	setActivePalette(palettes.palettes[palettes.defaults[userTheme]])
+
+	const paletteOptions = document.querySelectorAll("input[name=theme][type=radio]")
+
+	for(const paletteOption of paletteOptions) {
+		paletteOption.addEventListener('click', () => {
+			setActivePalette(palettes.palettes[paletteOption.value])
+		})
+	}
+}
+
 /**
  * Initialises the main page asynchronously.
  */
-export async function init() {
+async function init() {
 	icondata = await fetch(BASE_URL + "icondata.json").then(response => response.json())
 	palettes = await fetch(BASE_URL + "palettes.json").then(response => response.json())
 
 	await render.init()
 
-	alert("Loaded " + icondata.length + " icons and " + palettes.palettes.length + " palettes")
+	initPalettes()
 
 	document.querySelector("#page-loading").className = "done"
 }
